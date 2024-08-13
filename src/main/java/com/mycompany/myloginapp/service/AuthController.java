ade4cefe78c913845bf00e738d4c4b321eb37ab9
Hemanthbollina123 <hsbollina@gmail.com>
2024-08-13 14:20:59 -0400

    hwuf

--- src/main/java/com/mycompany/myloginapp/service/AuthController.java
+++ src/main/java/com/mycompany/myloginapp/service/AuthController.java
@@ -1,12 +1,12 @@
-package com.mycompany.myloginapp.controller;
+package com.mycompany.myloginapp.AuthController;
 
-import com.mycompany.myloginapp.model.User;
-import com.mycompany.myloginapp.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import com.mycompany.myloginapp.model.User;
+import com.mycompany.myloginapp.service.AuthService;
 
 @Controller
 public class AuthController {
@@ -14,11 +14,7 @@ public class AuthController {
     @Autowired
     private AuthService authService;
 
-    @GetMapping("/login")
-    public String showLoginForm(Model model) {
-        model.addAttribute("user", new User());
-        return "login";
-    }
+  
 
     @PostMapping("/login")
     public String login(User user, Model model) {
--- src/test/java/com/mycompany/myloginapp/TestCase.java
+++ src/test/java/com/mycompany/myloginapp/TestCase.java
@@ -2,4 +2,8 @@ package com.mycompany.myloginapp;
 
 public class TestCase {
 
+    public TestCase(String testName) {
+        //TODO Auto-generated constructor stub
+    }
+
 }
